28aff720c09aa653f4e80b626e20f5db3006ce41
rich <rich@richmiller.com>
2025-08-23 15:16:13 -0700

    bitwise manipulation this approach avoids conditional logic (like if-else statements) for each bit operation

--- register_rflags.cpp
+++ register_rflags.cpp
@@ -1,21 +1,19 @@
 #include "x86_simulator.h"
+bool X86Simulator::get_CF() const { return (rflags_ >> RFLAGS_CF_BIT) & 1; }
+void X86Simulator::set_CF(bool value) {
+  if (value) rflags_ |= (1ULL << RFLAGS_CF_BIT);
+  else rflags_ &= ~(1ULL << RFLAGS_CF_BIT);
+}
 
+bool X86Simulator::get_ZF() const { return (rflags_ >> RFLAGS_ZF_BIT) & 1; }
+void X86Simulator::set_ZF(bool value) {
+  if (value) rflags_ |= (1ULL << RFLAGS_ZF_BIT);
+  else rflags_ &= ~(1ULL << RFLAGS_ZF_BIT);
+}
 
-    bool X86Simulator::get_CF() const { return (rflags_ >> RFLAGS_CF_BIT) & 1; }
-    void X86Simulator::set_CF(bool value) {
-        if (value) rflags_ |= (1ULL << RFLAGS_CF_BIT);
-        else rflags_ &= ~(1ULL << RFLAGS_CF_BIT);
-    }
-
-    bool X86Simulator::get_ZF() const { return (rflags_ >> RFLAGS_ZF_BIT) & 1; }
-    void X86Simulator::set_ZF(bool value) {
-        if (value) rflags_ |= (1ULL << RFLAGS_ZF_BIT);
-        else rflags_ &= ~(1ULL << RFLAGS_ZF_BIT);
-    }
-
-    bool X86Simulator::get_SF() const { return (rflags_ >> RFLAGS_SF_BIT) & 1; }
-    void X86Simulator::set_SF(bool value) {
-        if (value) rflags_ |= (1ULL << RFLAGS_SF_BIT);
-        else rflags_ &= ~(1ULL << RFLAGS_SF_BIT);
-    }
+bool X86Simulator::get_SF() const { return (rflags_ >> RFLAGS_SF_BIT) & 1; }
+void X86Simulator::set_SF(bool value) {
+  if (value) rflags_ |= (1ULL << RFLAGS_SF_BIT);
+  else rflags_ &= ~(1ULL << RFLAGS_SF_BIT);
+}
 
